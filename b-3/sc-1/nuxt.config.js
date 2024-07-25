bca19f3c15ce0a43db81defdb286179408fe435f
Robby Pambudi <robby.pambudi10@gmail.com>
2024-07-25 22:04:02 +0700

    feat: Add build directory configuration to nuxt.config.js

--- b-3/sc-1/nuxt.config.js
+++ b-3/sc-1/nuxt.config.js
@@ -44,4 +44,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  // Build Directory
+  buildDir: 'dist',
+  generate: {
+    dir: 'dist',
+  },
 }
